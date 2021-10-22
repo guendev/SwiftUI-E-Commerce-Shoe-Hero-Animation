b6ab743803514f1a67e90c18c394d3fdde6ae44a
yuan <dnstylish@gmail.com>
2021-10-22 19:52:15 +0700

    Initial Commit

---
+++ Shared/E_CommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceApp.swift
+//  Shared
+//
+//  Created by Yuan on 22/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
